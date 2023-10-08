ae5b73dc424aba7de3bc57aa052c45326956329f
Ayush <ayush.kanduri@gmail.com>
2023-10-08 16:15:50 +0530

    Implemented Abort Controller

--- React Data Fetching/data-fetching-techniques/src/pages/Strategy1.jsx
+++ React Data Fetching/data-fetching-techniques/src/pages/Strategy1.jsx
@@ -1,14 +1,19 @@
 import UICard from "@/components/UICard";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Loader from "@/components/Loader";
 
 const Strategy1 = () => {
 	const [state, setState] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const abortController = useRef(new AbortController());
 	useEffect(() => {
 		const fetcher = async (...args) => {
 			try {
-				const res = await fetch(...args);
+				abortController.current?.abort();
+				abortController.current = new AbortController();
+				const signal = abortController.current.signal;
+
+				const res = await fetch(...args, { signal });
 				const data = await res.json();
 				setState(data);
 				setLoading(false);
@@ -18,6 +23,7 @@ const Strategy1 = () => {
 			}
 		};
 		fetcher(`https://jsonplaceholder.typicode.com/posts`);
+		return () => abortController.current?.abort();
 	}, []);
 	return (
 		<>
